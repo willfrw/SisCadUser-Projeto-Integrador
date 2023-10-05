9b686171a4195e4ad19b6d42e8e5cc89b76df0c3
willfrw <william.wang_dev@outlook.com>
2023-10-05 19:19:00 -0300

    uptdates

--- formulário de cadastro/assets/funções/teste-input-profile.js
+++ formulário de cadastro/assets/funções/teste-input-profile.js
@@ -1,7 +1,14 @@
-const inputFile = document.querySelector('.picture__input');
+const inputFile = document.querySelector('#picture__input');
 
-const pictureImage = document.querySelector('.profile__image');
+const pictureImage = document.querySelector('.picture__image');
 
 const pictureImageTxt = "Foto 3x4";
 
 pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener('change', function(e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+    console.log(file);
+});
